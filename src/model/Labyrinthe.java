5d96ce46fe2c16b30a4b30f8dd23c4cadc1be309
Formotiis <bryan.virte8@etu.univ-lorraine.fr>
2018-10-26 13:34:27 +0200

    Merge branch 'sprint-2' of /home/profil/virte1u/Espace-Etudiant/M1/ACL/Project/ACL2018-Codetonnix with conflicts.

--- src/model/Labyrinthe.java
+++ src/model/Labyrinthe.java
@@ -3,6 +3,7 @@ package model;
 import model.cases.*;
 
 import java.io.File;
+import java.util.Random;
 
 public class Labyrinthe {
 
@@ -12,12 +13,14 @@ public class Labyrinthe {
 
         cases = new Case[50][50];
 
+        Random r= new Random();
         for (int i=0;i<cases.length;i+=5){
             for (int j =0;j<cases[0].length;j+=5) {
-                metaTilesInit(1,i,j);
+                metaTilesInit(r.nextInt(11)+1,i,j);
             }
         }
 
+        //System.out.println(this); //affichage du labyrinthe généré
     }
 
     public void generer(File f){
@@ -26,19 +29,213 @@ public class Labyrinthe {
 
     private void metaTilesInit(int arg,int x,int y){
 
-        for (int i=0;i<5;i++){
-            for (int j =0;j<5;j++){
-                if (i%2==0&&j%2==0){
-                    cases[x+i][y+j]= new CaseMur(x+i,x+j);
-                } else {
-                    cases[x+i][y+j]= new CaseDefaut(x+i,x+j);
+
+        if (arg ==1) {
+            /** Tuile de la forme:(x murs, o default)
+             * x o x o x
+             * o o o o o
+             * x o x o x
+             * o o o o o
+             * x o x o x
+             */
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (i % 2 == 0 && j % 2 == 0) {
+                        cases[x + i][y + j] = new CaseMur(x + i, x + j);
+                    } else {
+                        cases[x + i][y + j] = new CaseDefaut(x + i, x + j);
+                    }
                 }
             }
+        } else if (arg ==2){
+            /** Tuile de la forme:(x murs, o default)
+             * x o x o x
+             * o o x o o
+             * x x x x x
+             * o o x o o
+             * x o x o x
+             */
+            metaTileLineInit(x,y,true,false,true, false,true);
+            metaTileLineInit(x+1,y,false,false,true, false,false);
+            metaTileLineInit(x+2,y,true,true,true, true,true);
+            metaTileLineInit(x+3,y,false,false,true, false,false);
+            metaTileLineInit(x+4,y,true,false,true, false,true);
+        } else if (arg ==3) {
+            /** Tuile de la forme:(x murs, o default)
+             * x o o o x
+             * o x x x o
+             * o x x x o
+             * o x x x o
+             * x o o o x
+             */
+            metaTileLineInit(x,y,true,false,false, false,true);
+            metaTileLineInit(x+1,y,false,true,true, true,false);
+            metaTileLineInit(x+2,y,false,true,true, true,false);
+            metaTileLineInit(x+3,y,false,true,true, true,false);
+            metaTileLineInit(x+4,y,true,false,false, false,true);
+
+        } else if (arg ==4) {
+            /** Tuile de la forme:(x murs, o default) ! Dangerous tile
+             * x x x o x
+             * o o o o x
+             * o x x x x
+             * o o x o o
+             * x o x o x
+             */
+            metaTileLineInit(x,y,true,true,true, false,true);
+            metaTileLineInit(x+1,y,false,false,false, false,true);
+            metaTileLineInit(x+2,y,false,true,true, true,true);
+            metaTileLineInit(x+3,y,false,false,true, false,false);
+            metaTileLineInit(x+4,y,true,false,true, false,true);
+
+        } else if (arg ==5) {
+            /** Tuile de la forme:(x murs, o default) ! Dangerous tile
+             * x o x x x
+             * x o o o o
+             * x x x x x
+             * o o o o x
+             * x x x o x
+             */
+            metaTileLineInit(x,y,true,false,true,true,true);
+            metaTileLineInit(x+1,y,true,false,false,false,false);
+            metaTileLineInit(x+2,y,true,true,true,true,true);
+            metaTileLineInit(x+3,y,false,false,false,false,true);
+            metaTileLineInit(x+4,y,true,false,true,true,true);
+
+        } else if (arg ==6) {
+            /** Tuile de la forme:(x murs, o default)
+             * x o x o x
+             * o o x o o
+             * x o x x x
+             * o o o o o
+             * x o x o x
+             */
+            metaTileLineInit(x,y,true,false,true, false,true);
+            metaTileLineInit(x+1,y,false,false,true,false,false);
+            metaTileLineInit(x+2,y,true,false,true,true,true);
+            metaTileLineInit(x+3,y,false,false,false,false,false);
+            metaTileLineInit(x+4,y,true,false,true,false,true);
+
+        } else if (arg ==7) {
+            /** Tuile de la forme:(x murs, o default)
+             * x o x o x
+             * o o x o o
+             * x o x o x
+             * o o x o o
+             * x o x o x
+             */
+            metaTileLineInit(x,y,true,false,true, false,true);
+            metaTileLineInit(x+1,y,false,false,true,false,false);
+            metaTileLineInit(x+2,y,true,false,true,false,true);
+            metaTileLineInit(x+3,y,false,false,true,false,false);
+            metaTileLineInit(x+4,y,true,false,true,false,true);
+
+        } else if (arg ==8) {
+            /** Tuile de la forme:(x murs, o default)
+             * x o x o x
+             * o o o o o
+             * x x x x x
+             * o o o o o
+             * x o x o x
+             */
+            metaTileLineInit(x,y,true,false,true, false,true);
+            metaTileLineInit(x+1,y,false,false,false, false,false);
+            metaTileLineInit(x+2,y,true,true,true,true,true);
+            metaTileLineInit(x+3,y,false,false,false,false,false);
+            metaTileLineInit(x+4,y,true,false,true,false,true);
+
+        } else if (arg ==9) {
+            /** Tuile de la forme:(x murs, o default)
+             * o o x o o
+             * o x x x o
+             * x x x x x
+             * o x x x o
+             * o o x o o
+             */
+            metaTileLineInit(x,y,false,false,true,false,false);
+            metaTileLineInit(x+1,y,false,true,true,true,false);
+            metaTileLineInit(x+2,y,true,true,true,true,true);
+            metaTileLineInit(x+3,y,false,true,true,true,false);
+            metaTileLineInit(x+4,y,false,false,true,false,false);
+
+        } else if (arg ==9) {
+            /** Tuile de la forme:(x murs, o default)
+             * o o x o o
+             * o x x x o
+             * x x x x x
+             * o x x x o
+             * o o x o o
+             */
+            metaTileLineInit(x,y,false,false,true,false,false);
+            metaTileLineInit(x+1,y,false,true,true,true,false);
+            metaTileLineInit(x+2,y,true,true,true,true,true);
+            metaTileLineInit(x+3,y,false,true,true,true,false);
+            metaTileLineInit(x+4,y,false,false,true,false,false);
+
+        } else if (arg ==10) {
+            /** Tuile de la forme:(x murs, o default)
+             * o o x o x
+             * o x o o o
+             * x o o o x
+             * o o o x o
+             * x o x o o
+             */
+            metaTileLineInit(x,y,false,false,true,false,true);
+            metaTileLineInit(x+1,y,false,true,false,false,false);
+            metaTileLineInit(x+2,y,true,false,false,false,true);
+            metaTileLineInit(x+3,y,false,false,false,true,false);
+            metaTileLineInit(x+4,y,true,false,true,false,false);
+
+        } else if (arg ==11) {
+            /** Tuile de la forme:(x murs, o default)
+             * x o x o o
+             * o o o x o
+             * x o o o x
+             * o x o o o
+             * o o x o x
+             */
+            metaTileLineInit(x,y,true,false,true,false,false);
+            metaTileLineInit(x+1,y,false,false,false,true,false);
+            metaTileLineInit(x+2,y,true,false,false,true,true);
+            metaTileLineInit(x+3,y,false,true,false,false,false);
+            metaTileLineInit(x+4,y,false,false,true,false,true);
+        } else {
+            System.out.println("Unknown argument: '"+arg+"'");
         }
 
+
+    }
+
+    /**
+     * Fonction d'initialisation d'une ligne d'une meta-tile (1x5)
+     * @param x x d'origine
+     * @param y y d'origine
+     * @param z Suite de boolens vrai s'il y a un mur faux sinon
+     */
+    private void metaTileLineInit(int x,int y, boolean ... z){
+        for (int i =0;i<z.length;i++){
+            if(z[i]){
+                cases[x][y+i]= new CaseMur(x,y+i);
+            } else {
+                cases[x][y+i] = new CaseDefaut(x,y+i);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<cases.length;i++){
+            for(int j=0;j<cases[0].length;j++){
+                sb.append(cases[i][j].toString()).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public Case[][] getCases() {
         return cases;
     }
+
 }
--- src/model/cases/CaseMur.java
+++ src/model/cases/CaseMur.java
@@ -10,4 +10,9 @@ public class CaseMur extends Case{
     void action() {
 
     }
+
+    @Override
+    public String toString() {
+        return "x";
+    }
 }
